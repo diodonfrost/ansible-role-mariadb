f6939ea1e49b8ea3171ee59ca8d57bd90966a9cb
diodonfrost <antoine.mace@protonmail.com>
2018-09-08 17:22:40 +0200

    use mariadb repository

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -1,10 +1,14 @@
 # Mariadb  install
 
+mariadb_package = 'mariadb-server'
+mariadb_package = 'MariaDB-server' if os.family ==  'redhat'
+mariadb_package = 'MariaDB-server' if os.name ==  'fedora'
+
 control 'install-01' do
   impact 1.0
   title 'Mariadb install'
   desc 'Mariadb should be installed'
-  describe package('mariadb-server') do
+  describe package(mariadb_package) do
     it { should be_installed }
   end
 end
