5b55909c8bd01b4d4eaf52314599838c6a9fa63a
diodonfrost <antoine.mace@protonmail.com>
2018-09-11 04:19:24 +0200

    add service test

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -1,9 +1,13 @@
 # Mariadb  install
 
 mariadb_package = 'mariadb-server'
-mariadb_package = 'MariaDB-server' if os.family == 'redhat'
-mariadb_package = 'MariaDB-server' if os.name == 'fedora'
+mariadb_package = 'MariaDB-server' if os.family == 'redhat' && os[:release].to_i >= 6
+mariadb_package = 'MariaDB-server' if os.name == 'fedora' && os[:release].to_i >= 27
 mariadb_package = 'MariaDB-server' if os.family == 'suse'
+
+mariadb_service = 'mariadb'
+mariadb_service = 'mysql' if os.family == 'redhat' && os[:release].to_i <= 6
+mariadb_service = 'mysql' if os.family == 'debian' && os[:release].to_i <= 14
 
 control 'install-01' do
   impact 1.0
@@ -11,5 +15,15 @@ control 'install-01' do
   desc 'Mariadb should be installed'
   describe package(mariadb_package) do
     it { should be_installed }
+  end
+end
+
+control 'install-02' do
+  impact 1.0
+  title 'Mariadb service'
+  desc 'Mariadb should be running and enabled'
+  describe service(mariadb_service) do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
