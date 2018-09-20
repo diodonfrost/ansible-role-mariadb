755df988da2ddd81d4d909e31a9fa7c6db6cba3f
diodonfrost <antoine.mace@protonmail.com>
2018-09-20 19:24:59 +0200

    add inspec mysql query tests

--- tests/inspec/database_spec.rb
+++ tests/inspec/database_spec.rb
@@ -8,3 +8,30 @@ control 'database-01' do
     its('exit_status') { should eq(0) }
   end
 end
+
+control 'database-02' do
+  impact 1.0
+  title 'Display database test'
+  desc 'Mariadb database test should be exist'
+  describe mysql_session('root').query('show databases like "test";') do
+    its('stdout') { should match(/test/) }
+  end
+end
+
+control 'database-03' do
+  impact 1.0
+  title 'Test for a successful query'
+  desc 'Mariadb database query should  be functional'
+  describe mysql_session('root').query('show tables in test;') do
+    its('exit_status') { should eq(0) }
+  end
+end
+
+control 'database-04' do
+  impact 1.0
+  title 'Test for a faiing query'
+  desc 'Mariadb database query should not be functional'
+  describe mysql_session('root').query('show tables in non_existent_database;') do
+    its('exit_status') { should_not eq(0) }
+  end
+end
