9b5fa07a149fa1f20592da660cce942d7de32be0
diodonfrost <antoine.mace@protonmail.com>
2018-09-25 05:24:05 +0200

    add openbsd support

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -9,7 +9,8 @@ mariadb_service = 'mariadb'
 mariadb_service = 'mysql' if os.name == 'centos' or  os.name == 'redhat' && os[:release].to_i <= 6
 mariadb_service = 'mysql' if os.family == 'debian' && os[:release].to_i <= 14
 mariadb_service = 'mariadb' if os.name == 'Amazon Linux' && os[:release].to_i >= 2
-mariadb_service = 'mysql-server' if os.family == 'bsd'
+mariadb_service = 'mysql-server' if os.name == 'freebsd'
+mariadb_service = 'mysqld' if os.name == 'openbsd'
 
 control 'install-01' do
   impact 1.0
