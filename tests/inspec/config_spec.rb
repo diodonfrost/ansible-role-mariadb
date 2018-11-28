971eb8ebf35b2741141ae78a223c26d5ebacd272
diodonfrost <diodon.frost@diodonfrost.me>
2018-11-28 18:29:31 +0100

    refactoring code

--- tests/inspec/config_spec.rb
+++ tests/inspec/config_spec.rb
@@ -1,9 +1,7 @@
 # Mariadb  configuration tests
 
-mariadb_service = 'mariadb'
-mariadb_service = 'mysql' if os.name == 'centos' or  os.name == 'redhat' && os[:release].to_i <= 6
-mariadb_service = 'mysql' if os.family == 'debian' && os[:release].to_i <= 14
-mariadb_service = 'mariadb' if os.name == 'Amazon Linux' && os[:release].to_i >= 2
+mariadb_process = 'mysqld'
+mariadb_process = '/var/run/mysql/mysql.sock' if os.name == 'openbsd'
 
 control 'config-01' do
   impact 1.0
@@ -11,6 +9,6 @@ control 'config-01' do
   desc 'Mariadb should be listenning on 3306 port'
   describe port(3306) do
     it { should be_listening }
-    its('processes') {should include 'mysqld'}
+    its('processes') {should include mariadb_process}
   end
 end
